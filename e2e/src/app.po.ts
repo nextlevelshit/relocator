57bfd0c1fe968f290591d4d17fdea1681c56250e
Michael Czechowski <mail@dailysh.it>
2018-07-18 15:49:59 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('nls-root h1')).getText();
+  }
+}
